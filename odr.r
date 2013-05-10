d65d526efa46f908d0a7376c0b75b7fa42be92ac
Christopher Tong <hydrodynamic.stability@yahoo.com>
2013-05-10 00:18:00 -0400

    Created a new R script containing function odr()
    This function is used to calculate a simple linear regression when both variables are random, and have the same physical dimension and units.  It returns slope and intercept of the orthogonal distance regression line (first principal component), eigenvalues, eccentricity of elliptical contour, and Coleman correlation.

---
+++ odr.r
@@ -0,0 +1,59 @@
+##########################################################################
+###
+### R function for simple linear regression when both variables are random,
+### and both variables have the same physical dimensions and units.
+### Calculate the first principal component (Orthogonal distance regression line)
+### Return eigenvalues and Coleman correlation
+### See C. Tong (2011), "Concordance correlation decomposed into the
+### product of precision and accuracy", JSM 2011 Proceedings, Biometrics Section, pp. 649-662.
+### 
+### Christopher Tong 9 May 2013
+###
+##########################################################################
+
+# Assume x and y are numeric vectors of the same length, representing paired data.
+
+odr <- function(x,y)
+{
+    # Check inputs
+    x.numeric <- is.numeric(x)
+    y.numeric <- is.numeric(y)
+    xy.same.length <- length(x) == length(y)
+    
+    if (x.numeric & y.numeric & xy.same.length) {
+    
+        # Remove NAs
+        x.nans <- is.na(x)
+        y.nans <- is.na(y)
+        all.nans <- x.nans | y.nans
+        new.x <- x[!all.nans]
+        new.y <- y[!all.nans]
+        
+        x <- new.x
+        y <- new.y
+
+        # Variances and covariances
+        syy <- var(y)
+        sxx <- var(x)
+        sxy <- cov(x,y,use="pairwise.complete.obs")
+    
+        # Slope (m2) and intercept (int) of ODR line:
+        m2 <- (syy - sxx + sqrt((syy - sxx)^2 + 4*sxy^2))/(2*sxy)
+        int <- mean(y) - m2*mean(x)
+    
+        # Eigenvalues
+        eval1 <- (syy + sxx + sqrt((syy - sxx)^2 + 4*sxy^2))/2
+        eval2 <- (syy + sxx - sqrt((syy - sxx)^2 + 4*sxy^2))/2
+    
+        # Ratio of square roots of eigenvalues (called gof here); eccentricity
+        gof <- sqrt(eval2/eval1)
+        eccentricity <- sqrt(1 - (eval2/eval1))
+    
+        # Coleman correlation (rc) on raw data
+        rc <- 1 - sqrt(eval2/eval1)
+    
+        list(int=int,slope=m2,gof=gof,eccentricity=eccentricity,eval1=eval1,eval2=eval2,rc=rc)
+    } else {
+        print("Input error")
+    }
+}
